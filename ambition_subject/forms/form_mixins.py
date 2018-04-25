51863a4fa4e2f19e403ab799bf12c68b31f42fb3
Leslie <leslie.kep@gmail.com>
2018-04-25 17:42:01 +0200

    add VisitTrackingModelFormMixin

--- ambition_subject/forms/form_mixins.py
+++ ambition_subject/forms/form_mixins.py
@@ -3,11 +3,13 @@ import arrow
 from django import forms
 from edc_base.sites import SiteModelFormMixin
 from edc_form_validators import FormValidatorMixin
+from edc_visit_tracking.modelform_mixins import VisitTrackingModelFormMixin
 
 from ..models import SubjectVisit
 
 
-class SubjectModelFormMixin(SiteModelFormMixin, FormValidatorMixin, forms.ModelForm):
+class SubjectModelFormMixin(SiteModelFormMixin, VisitTrackingModelFormMixin,
+                            FormValidatorMixin, forms.ModelForm):
 
     visit_model = SubjectVisit
 
