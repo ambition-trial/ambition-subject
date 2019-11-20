d3deb38f56c239f7e6a6d6d393ff5bd610c9f4b4
erikvw <ew2789@gmail.com>
2019-11-20 14:07:43 -0600

    update perm inports

--- runtests.py
+++ runtests.py
@@ -64,7 +64,7 @@ DEFAULT_SETTINGS = DefaultTestSettings(
         "edc_timepoint.apps.AppConfig",
         "edc_visit_schedule.apps.AppConfig",
         "edc_visit_tracking.apps.AppConfig",
-        "ambition_permissions.apps.AppConfig",
+        "ambition_auth.apps.AppConfig",
         "ambition_labs.apps.AppConfig",
         "ambition_lists.apps.AppConfig",
         "ambition_ae.apps.AppConfig",
